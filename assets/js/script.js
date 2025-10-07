c311db1795cb28222fdbce88787e5512ee9313a0
Michecosa <micheladellagatta1@gmail.com>
2025-10-07 14:17:26 +0200

    Add comment before for

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,11 @@
   Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 */
 
-
+// Ciclo da 1 a 100 per stampare:
+// - "Fizz" per i multipli di 3
+// - "Buzz" per i multipli di 5
+// - "FizzBuzz" per i multipli di entrambi
+// Altrimenti, stampo il numero
 for(let i=1; i<=100; i++) {
   // Calcolo una sola volta le due condizioni di divisibilità
   const divisibleBy3 = i % 3 == 0; // true se i è multiplo di 3
@@ -21,7 +25,7 @@ for(let i=1; i<=100; i++) {
   // Altrimenti, se è solo divisibile per 5, stampo "Buzz"
   } else if( divisibleBy5 ) {
     console.log("Buzz");
-    
+
   // Altrimeniti, stampo il numero (nessuna delle condizioni precedenti è vera)
   } else {
     console.log(i);
